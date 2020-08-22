387d6d38dca8ab9005c909b0399a3db68f02e4f1
Majd Hailat <majdhailat64@gmail.com>
2020-08-22 13:52:09 -0400

    add ejs

--- app.js
+++ app.js
@@ -4,19 +4,44 @@ const bodyParser = require("body-parser");
 const app = express();
 app.set('view engine', 'ejs');
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
 
   var date = new Date();
   var today = date.getDay();
+  var day = "";
 
-  if (today === 6 || today === 0){
-    res.sendFile(__dirname + "/index.html");
-  }else{
-    res.send("<h1>Ohhhhhh shit</h1>");
+  switch (today) {
+    case 0:
+      day = "sudnay";
+    break;
+    case 1:
+      day = "Monday"
+    break;
+    case 2:
+      day = "Tuesday"
+    break;
+    case 3:
+      day = "Wednesday"
+    break;
+    case 4:
+      day = "Thursday"
+    break;
+    case 5:
+      day = "Friday"
+    break;
+    case 6:
+      day = "Saturday"
+    break;
+    default:
+    console.log("error: " + today);
   }
+
+  res.render("list", {
+    day: day
+  });
 
 });
 
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("Server started on port 3000.");
 });
